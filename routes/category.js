7a13accdb4d28afc496555cb718b53dd3608254e
Ssendikadiwa Stephen <ssendikadiwastephen@Ssendikadiwas-MacBook-Pro.local>
2020-12-09 19:22:18 +0300

    Add functionality to get all categories

--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { create, update, remove } = require('../controllers/category');
+const { create, update, remove, list } = require('../controllers/category');
 const { runValidation } = require('../validator');
 const { categoryCreateValidator } = require('../validator/category');
 
@@ -10,6 +10,7 @@ router.post(
   runValidation,
   create,
 );
+router.get('/categories', list);
 router.put('/category/:id', categoryCreateValidator, runValidation, update);
 router.delete('/category/:id', remove);
 
