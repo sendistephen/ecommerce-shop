dcac2e67b9e29f921bf9e4ef171be8921b2d78ba
Ssendikadiwa Stephen <ssendikadiwastephen@Ssendikadiwas-MacBook-Pro.local>
2020-12-16 17:12:40 +0300

    Add signin route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('../middleware/async');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 exports.create = asyncHandler(async (req, res) => {
   if (!req.body.email || !req.body.password) {
@@ -24,5 +25,28 @@ exports.create = asyncHandler(async (req, res) => {
     user.salt = undefined;
     user.hashed_password = undefined;
     res.json({ user });
+  });
+});
+
+exports.signin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  // find user by email
+  await User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res
+        .status(400)
+        .json({ error: 'User with that email does not exist' });
+    }
+    // check if user is authenticated
+    if (!user.authenticate(password)) {
+      return res.status(401).json({ error: 'Email or password is incorrect' });
+    }
+    // generate token
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    // persist token in cookie
+    res.cookie('t', token, { expire: new Date() + 9999 });
+    // return response
+    const { _id, email, username } = user;
+    return res.json({ token, user: { _id, email, username } });
   });
 });
